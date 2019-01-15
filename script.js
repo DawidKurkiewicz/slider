a36df2201050f8b5a5982b9607535e4a31950078
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-15 12:24:50 +0100

    started changing slides on dots on the bottom

--- script.js
+++ script.js
@@ -13,8 +13,13 @@ const slideList = [{
 
 const image = document.querySelector("img.slider")
 const h1 = document.querySelector("h1.slider")
+const dots = [...document.querySelectorAll(".dots span")]
 const time = 5000;
 let active = 0;
+
+const changeDot = () => {
+
+}
 
 const changeSlide = () => {
     active++
@@ -23,6 +28,7 @@ const changeSlide = () => {
     }
     image.src = slideList[active].img
     h1.textContent = slideList[active].text
+    changeDot()
 }
 
 setInterval(changeSlide, time)
