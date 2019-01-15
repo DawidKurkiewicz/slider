44c5eb275a002ed55196e947f9b1176c382db680
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-15 13:57:38 +0100

    keydowns works

--- script.js
+++ script.js
@@ -18,14 +18,14 @@ const time = 5000;
 let active = 0;
 
 const changeDot = () => {
-const activeDot = dots.findIndex(dot => dot.classList.contains("active"));
-dots[activeDot].classList.remove("active")
-dots[active].classList.add("active")
+    const activeDot = dots.findIndex(dot => dot.classList.contains("active"));
+    dots[activeDot].classList.remove("active")
+    dots[active].classList.add("active")
 }
 
 const changeSlide = () => {
     active++
-    if(active === slideList.length) {
+    if (active === slideList.length) {
         active = 0;
     }
     image.src = slideList[active].img
@@ -51,14 +51,24 @@ const third = () => {
     h1.textContent = slideList[active].text
     changeDot()
 }
-document.getElementById("one").addEventListener("click", first )
-document.getElementById("two").addEventListener("click", second )
-document.getElementById("three").addEventListener("click", third )
+document.getElementById("one").addEventListener("click", first)
+document.getElementById("two").addEventListener("click", second)
+document.getElementById("three").addEventListener("click", third)
 
 setInterval(changeSlide, time)
 
 const keyChange = (e) => {
-
+    if (e.keyCode == 37 || e.keyCode == 39) {
+        e.keyCode == 37 ? active-- : active++
+        if (active === slideList.length) {
+            active = 0;
+        } else if (active < 0) {
+            active = slideList.length - 1
+        }
+        image.src = slideList[active].img
+        h1.textContent = slideList[active].text
+        changeDot()
+    }
 }
 
 window.addEventListener("keydown", keyChange)
