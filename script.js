2c55e0fbd86fc21187db695b983515308a411bcd
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-15 12:00:18 +0100

    added styles, html strcture and slideList in js

---
+++ script.js
@@ -0,0 +1,12 @@
+const slideList = [{
+    img: "images/img1.jpg",
+    text: "First Slide"
+},
+{
+    img: "images/img2.jpg",
+    text: "Second Slide"
+},
+{
+    img: "images/img3.jpg",
+    text: "Third Slide"
+}]
