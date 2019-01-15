e2cabc996450eebdbb796c50e92ee4ed043f345f
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-15 12:05:08 +0100

    changing slides by setInterval

--- script.js
+++ script.js
@@ -9,4 +9,20 @@ const slideList = [{
 {
     img: "images/img3.jpg",
     text: "Third Slide"
-}]
+}];
+
+const image = document.querySelector("img.slider")
+const h1 = document.querySelector("h1.slider")
+const time = 5000;
+let active = 0;
+
+const changeSlide = () => {
+    active++
+    if(active === slideList.length) {
+        active = 0;
+    }
+    image.src = slideList[active].img
+    h1.textContent = slideList[active].text
+}
+
+setInterval(changeSlide, time)
